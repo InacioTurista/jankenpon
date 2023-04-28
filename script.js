3529fcc72c7700c104ddbb5704aac4fdd4c8b103
InacioTurist <gabriel.inaciofn@gmail.com>
2023-04-28 20:33:22 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const options = ["rock", "paper", "scissor"];
+
+let yourPick = options[Math.floor(Math.random() * 2)];
+let myPick;
+let score;
+var previousImage = null;
+
+document.getElementById();
+
+function selectYourPick(option) {
+  yourPick = option;
+  myPick = options[Math.floor(Math.random() * 2)];
+
+  if (
+    (yourPick == "rock" && myPick == "paper") ||
+    (yourPick == "paper" && myPick == "scissor") ||
+    (yourPick == "scissor" && myPick == "rock")
+  ) {
+    score = "I Won!";
+  } else if (
+    (yourPick == "rock" && myPick == "scissor") ||
+    (yourPick == "paper" && myPick == "rock") ||
+    (yourPick == "scissor" && myPick == "paper")
+  ) {
+    score = "You Won!";
+  } else {
+    score = "Draw!";
+  }
+}
+
+function image(src) {
+  // This will check if there is any img, if true it will be deleted before putting the new one
+  if (previousImage !== null) {
+    previousImage.remove();
+  }
+
+  var img = document.createElement("img");
+  img.src = src;
+  img.classList.add("imageFeatures");
+  document.body.appendChild(img);
+
+  // To update my previousImage
+  previousImage = img;
+}
