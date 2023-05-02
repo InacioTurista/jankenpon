749fd17b784fa2044623934c8e17b6e41cb4ea8f
InacioTurist <gabriel.inaciofn@gmail.com>
2023-05-02 00:19:44 -0300

    A few corrections, I think now it's finally completed

--- script.js
+++ script.js
@@ -1,52 +1,85 @@
 //Sides and result variables
 
 const myPickOptions = {
-  "rock": "images/rock.png",
-  "paper": "images/paper.png",
-  "scissor": "images/scissor.png"
+  rock: "images/rock.png",
+  paper: "images/paper.png",
+  scissor: "images/scissor.png",
 };
 const results = {
-  "draw": "images/draw.png",
-  "win": "images/win.png",
-  "lose": "images/lose.png"
+  draw: "images/draw.png",
+  win: "images/win.png",
+  lose: "images/lose.png",
 };
 const myPickImgs = Object.values(myPickOptions);
 let yourPick;
 let myPick;
 let score;
+let count = 0;
 
 //--------------- Responsible for setting the images ----------------------//
 
 function image(src, local) {
   let adiv = document.getElementById(local);
+
+  // Added this if statement to hide all the images in the right moment
+  if (count % 3 === 0) {
+    document.getElementById("your-img").classList.remove("imageFeatures");
+    document.getElementById("my-img").classList.remove("imageFeatures");
+    document.getElementById("result-img").classList.remove("imageFeatures");
+    document.getElementById("your-img").classList.add("hidden");
+    document.getElementById("my-img").classList.add("hidden");
+    document.getElementById("result-img").classList.add("hidden");
+  }
+
   adiv.src = src;
+
+  // Added this setTimeout so the image stays a little time hidden before it's shown again.
+  setTimeout(function () {
+    adiv.classList.remove("hidden");
+  }, 400);
+
   adiv.classList.add("imageFeatures");
 
+  count++;
 }
 
 //-------------------- yourPick's Event listeners --------------------//
 
-let choice = document.getElementsByClassName('choices');
+let choice = document.getElementsByClassName("choices");
 let choiceArr = [...choice];
-console.log(choiceArr)
-choiceArr.map(a => a.addEventListener("click", () =>{ return image(a.src, "your-img"), result(choiceArr.indexOf(a))}));
+console.log(choiceArr);
+choiceArr.map((a) =>
+  a.addEventListener("click", () => {
+    return image(a.src, "your-img"), result(choiceArr.indexOf(a));
+  })
+);
 //--------------------- ------------------------ --------------------//
 
 //Result and myPick's choice print
 
 function result(yourOption) {
-  
   let random = [Math.round(Math.random() * 2)];
   yourPick = yourOption;
   myPick = random;
-  image(myPickImgs[random], 'my-img');
 
-// ------------------------------------------------ //
-let vs = yourPick - myPick;
-// vs equals 2 || -1 print "I won";
-// vs equals -2 || 1 print "You won";
-// vs == 0 print "Draw";
-vs == 0 ? score = "draw" : vs == 2 || vs == -1 ? score = "win" : score = "lose";
+  // Added this setTimeout so it delays when my-img it's shown.
+  setTimeout(function () {
+    image(myPickImgs[random], "my-img");
+  }, 800);
 
-image(results[score], 'result-img');
+  // ------------------------------------------------ //
+  let vs = yourPick - myPick;
+  // vs equals 2 || -1 print "I won";
+  // vs equals -2 || 1 print "You won";
+  // vs == 0 print "Draw";
+  vs == 0
+    ? (score = "draw")
+    : vs == 2 || vs == -1
+    ? (score = "lose")
+    : (score = "win");
+
+  // Lastly, I added another setTimeout to delay when result-img it's shown.
+  setTimeout(function () {
+    image(results[score], "result-img");
+  }, 1400);
 }
